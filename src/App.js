689dc103f7b71b0e15a59f4f1f0293b961a37323
Carson Brown <carsonb@ifit.com>
2021-10-04 14:30:06 -0400

    add rotating ship, clean up cra

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useLoader, Canvas, useFrame } from "@react-three/fiber";
+import React, { Suspense, useRef } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import "./styles.css";
 
-function App() {
+function Loading() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
+      <sphereGeometry attach="geometry" args={[1, 16, 16]} />
+      <meshStandardMaterial
+        attach="material"
+        color="white"
+        transparant
+        opacity={0.6}
+        roughness={1}
+        metalness={0}
+      />
+    </mesh>
   );
 }
 
-export default App;
+function ArWing() {
+  const group = useRef();
+  const { nodes } = useLoader(GLTFLoader, "models/arwing.glb");
+  useFrame(() => {
+    group.current.rotation.y += 0.004;
+  });
+  return (
+    <group ref={group}>
+      <mesh visible geometry={nodes.Default.geometry}>
+        <meshStandardMaterial
+          attach="material"
+          color="white"
+          roughness={0.3}
+          metalness={0.3}
+        />
+      </mesh>
+    </group>
+  );
+}
+
+export default function App() {
+  return (
+    <Canvas style={{ background: "#171717" }}>
+      <directionalLight intensity={0.5} />
+      <Suspense fallback={<Loading />}>
+        <ArWing />
+      </Suspense>
+    </Canvas>
+  );
+}
